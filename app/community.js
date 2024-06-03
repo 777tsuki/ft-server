f3af8fe1de1634566d40f696cdaac953831a9f90
和祯_BT <0@7tsuki.fun>
2024-06-03 18:58:07 +0800

    d

--- app/community.js
+++ app/community.js
@@ -4,7 +4,10 @@ const community = {
     let result = await User.find().toArray();
     result = result.filter(account => account.user != req.user);
     let records = [];
-    result.map(account => records.push(...account.archive.record));
+    result.map(account => {
+      account.archive.record.map(r => r.user = account.user);
+      records.push(...account.archive.record);
+    });
     records = records.filter(r => r.name.includes("通关"));
     return records;
   }
