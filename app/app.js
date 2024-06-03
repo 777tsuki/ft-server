9909dde1af6362a86ef0bfbf404d5d3415968103
和祯_BT <0@7tsuki.fun>
2024-06-03 16:46:28 +0800

    d

--- app/app.js
+++ app/app.js
@@ -10,10 +10,9 @@ const handle = {
 const app = new Koa();
 app.use(async (ctx, next) => {
   console.log(ctx.url);
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  console.log(ctx.method)
+  // ctx.set('Access-Control-Allow-Origin', '*');
+  // ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (ctx.method == 'OPTIONS') {
     ctx.body = 200;
     ctx.status = 204;
